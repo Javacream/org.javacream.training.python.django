72d8ee7f51e2f2ddc771e3527fe9b94143c6782d
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2026-01-06 19:15:41 +0100

    static html

--- training/demo/urls.py
+++ training/demo/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('demo/', views.demo_view, name='demo'),
+    path('demo/simple/', views.demo_view, name='demo_simple'),
+    path('demo/html/', views.demo_html, name='demo_html'),
 ]
--- training/demo/views.py
+++ training/demo/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
 
 def demo_view(request):
     return HttpResponse('demo_view is working')
+def demo_html(request):
+    simple_page = loader.get_template('simple.html')
+    return HttpResponse(simple_page.render())
