e36d2f76dd5dd711a79a63d1e2356e4271937b41
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2026-01-07 12:14:22 +0100

    delete untypical views

--- training/demo/urls.py
+++ training/demo/urls.py
@@ -1,7 +1,5 @@
 from . import views
 from django.urls import path
 urlpatterns = [
-    path('demo_plain/', views.demo_view, name='demo_plain'),
-    path('demo_http/', views.demo_http, name='demo_http'),
-    path('demo_http_template/', views.demo_http_template, name='demo_http_template'),
+    path('demo/', views.demo_http_template, name='demo_http_template'),
 ]
