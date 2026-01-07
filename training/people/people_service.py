1f273ec434938814bd542da84741d728af1b4f2b
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2026-01-07 15:15:15 +0100

    user from RESTful webservice

--- training/people/people_service.py
+++ training/people/people_service.py
@@ -1,1 +1,2 @@
-context = {'user': {'lastname': 'Sawitzki', 'firstname': 'Rainer'}}
+import requests
+context = {'user': requests.get('http://javacream.eu:8080/people/6').json(), 'app_name': 'P E O P L E', 'company': 'Javacream'}
