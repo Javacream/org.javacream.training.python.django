a92ab5c6ecd9f3dffb95a66c330a50e0f364b04c
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2026-01-07 15:39:38 +0100

    add people content

--- training/people/people_service.py
+++ training/people/people_service.py
@@ -1,2 +1,2 @@
 import requests
-context = {'user': requests.get('http://javacream.eu:8080/people/6').json(), 'app_name': 'P E O P L E', 'company': 'Javacream'}
+context = {'people': requests.get('http://javacream.eu:8080/people').json(), 'user': requests.get('http://javacream.eu:8080/people/6').json(), 'app_name': 'P E O P L E', 'company': 'Javacream'}
