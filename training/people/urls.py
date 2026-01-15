159f543bf0a1e7bedc97e6806a50f8c1ccce7cb3
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2026-01-15 15:49:10 +0100

    simple person input

--- training/people/urls.py
+++ training/people/urls.py
@@ -2,4 +2,5 @@ from . import views
 from django.urls import path
 urlpatterns = [
     path('people/', views.people, name='people'),
+    path('people/<str:page>/', views.people, name='people'),
 ]
--- training/people/views.py
+++ training/people/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
 from .people_service import get_context_for_people_list, get_context_for_person
+from django.http import HttpRequest
+from .forms import PersonInputForm
+
 # Create your views here.
 
-def people(request):
+def people(request: HttpRequest, page=''):
     id = request.GET.get('id')
     if id:
         context = get_context_for_person(id)
         context['page'] = 'person'
     else:
-        context = get_context_for_people_list()
-        context['page'] = 'list'
+        if page == 'peopleList':
+            context = get_context_for_people_list()
+            context['page'] = 'list'
+        else:
+            if request.method == "POST":
+                form = PersonInputForm(request.POST)
+                if form.is_valid():
+                    first_name = form.cleaned_data["first_name"]
+                    last_name = form.cleaned_data["last_name"]
+                    print(first_name, last_name)
+
+            else:
+                form = PersonInputForm()
+            context = {'page': 'person_input', 'form': form}
     return render(request, 'people.html', context)
+
 
