8d41dce9cfb670fcbe05e34a175f043e721ee311
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2026-01-15 14:01:56 +0100

    person details

--- training/people/people_service.py
+++ training/people/people_service.py
@@ -1,3 +1,10 @@
 import requests
 from .global_links import global_links
-context = {'links': global_links, 'people': requests.get('http://javacream.eu:8080/people').json(), 'user': requests.get('http://javacream.eu:8080/people/6').json(), 'app_name': 'P E O P L E', 'company': 'Javacream'}
+
+def get_context_for_people_list():    
+    context = {'links': global_links, 'people': requests.get('http://javacream.eu:8080/people').json(), 'user': requests.get('http://javacream.eu:8080/people/6').json(), 'app_name': 'P E O P L E', 'company': 'Javacream'}
+    return context
+
+def get_context_for_person(id):    
+    context = {'links': global_links, 'person': requests.get(f'http://javacream.eu:8080/people/{id}').json(), 'user': requests.get('http://javacream.eu:8080/people/6').json(), 'app_name': 'P E O P L E', 'company': 'Javacream'}
+    return context
--- training/people/views.py
+++ training/people/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-from .people_service import context
+from .people_service import get_context_for_people_list, get_context_for_person
 # Create your views here.
 
 def people(request):
+    id = request.GET.get('id')
+    if id:
+        context = get_context_for_person(id)
+        context['page'] = 'person'
+    else:
+        context = get_context_for_people_list()
+        context['page'] = 'list'
     return render(request, 'people.html', context)
+
